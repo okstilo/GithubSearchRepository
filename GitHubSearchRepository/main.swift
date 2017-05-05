c0cef12364eec082d65cda8f68fa0d4577c88f06
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-05-05 14:05:47 +0900

    Add xcode project

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by Masaki Kitagawa on 2017/05/05.
+//  Copyright © 2017年 okstilo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
