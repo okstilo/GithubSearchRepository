bc08ba23ab630fec4f7fe6d0a831cdf4719b798e
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-05-07 01:32:30 +0900

    Add FeildError to GithubAPIError

--- GitHubSearchRepository/GithubAPIError.swift
+++ GitHubSearchRepository/GithubAPIError.swift
@@ -9,7 +9,33 @@
 import Foundation
 
 struct GithubAPIError: JSONDecodable, Error {
+    struct FieldError: JSONDecodable {
+        let resource: String
+        let field: String
+        let code: String
+        
+        init(json: Any) throws {
+            guard let dict = json as? [String : Any] else {
+                throw JSONDecodeError.invalidFormat(json: json)
+            }
+            guard let resource = dict["resource"] as? String else {
+                throw JSONDecodeError.missingValue(key: "resource", actualValue: dict["resource"])
+            }
+            guard let field = dict["field"] as? String else {
+                throw JSONDecodeError.missingValue(key: "field", actualValue: dict["field"])
+            }
+            guard let code = dict["code"] as? String else {
+                throw JSONDecodeError.missingValue(key: "code", actualValue: dict["code"])
+            }
+            
+            self.resource = resource
+            self.field = field
+            self.code = code
+        }
+    }
+    
     let message: String
+    let fieldErrors: [FieldError]
     
     init(json: Any) throws {
         guard let dict = json as? [String : Any] else {
@@ -19,6 +45,10 @@ struct GithubAPIError: JSONDecodable, Error {
             throw JSONDecodeError.missingValue(key: "message", actualValue: dict["message"])
         }
         
+        let fieldErrorObjects = dict["errors"] as? [Any] ?? []
+        let filedErrors = try fieldErrorObjects.map { try FieldError(json: $0) }
+        
         self.message = message
+        self.fieldErrors = filedErrors
     }
 }
