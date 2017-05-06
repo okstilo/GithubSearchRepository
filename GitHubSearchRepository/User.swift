2924896548a219dab8f8c177f220df1a00c23259
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-05-06 23:17:28 +0900

    Add User

---
+++ GitHubSearchRepository/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  GitHubSearchRepository
+//
+//  Created by Masaki Kitagawa on 2017/05/06.
+//  Copyright © 2017年 okstilo. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let id: Int
+    let login: String
+}
