3c6d64868824a27484a6542dfe7aa9e9a7e54e15
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-05-06 23:19:32 +0900

    Add Repository

---
+++ GitHubSearchRepository/Repository.swift
@@ -0,0 +1,16 @@
+//
+//  Repository.swift
+//  GitHubSearchRepository
+//
+//  Created by Masaki Kitagawa on 2017/05/06.
+//  Copyright © 2017年 okstilo. All rights reserved.
+//
+
+import Foundation
+
+struct Repository {
+    let id: Int
+    let name: String
+    let fullName: String
+    let owner: User
+}
