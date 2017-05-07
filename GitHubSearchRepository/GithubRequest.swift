048a95349d6d930601cb8e994de211c0b16102c9
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-05-07 23:45:26 +0900

    Add build url request to GithubRequest

--- GitHubSearchRepository/GithubRequest.swift
+++ GitHubSearchRepository/GithubRequest.swift
@@ -21,4 +21,26 @@ extension GithubRequest {
     var baseURL: URL {
         return URL(string: "https://api.github.com")!
     }
+    
+    func buildURLRequest() -> URLRequest {
+        let url = baseURL.appendingPathComponent(path)
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        
+        switch method {
+        case .get:
+            let dict = parameters as? [String : Any]
+            let queryItems = dict?.map { key, value in
+                return URLQueryItem(name: key, value: String(describing: value))
+            }
+            components?.queryItems = queryItems
+        default:
+            fatalError("Unsupported method \(method)")
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.url = components?.url
+        urlRequest.httpMethod = method.rawValue
+        
+        return urlRequest
+    }
 }
