7f195f0b2cbc566221bf410e5820a93d88b3ce1b
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2017-05-08 01:20:39 +0900

    Add send to GithubClient

--- GitHubSearchRepository/GithubClient.swift
+++ GitHubSearchRepository/GithubClient.swift
@@ -17,8 +17,28 @@ class GithubClient {
     
     func send<Request: GithubRequest>(
         request: Request,
-        completion: ((Result<Request.Response, GithubClientError>) -> Void)) {
+        completion: @escaping ((Result<Request.Response, GithubClientError>) -> Void)) {
         
-        // WIP
+        let urlRequest = request.buildURLRequest()
+        let task = session.dataTask(with: urlRequest) { data, response, error in
+            switch (data, response, error) {
+            case (_, _, let error?):
+                completion(Result(error: .connectionError(error)))
+            case (let data?, let response?, _):
+                do {
+                    let response = try request.response(from: data, urlResponse: response)
+                    completion(Result(value: response))
+                }
+                catch let error as GithubAPIError {
+                    completion(Result(error: .apiError(error)))
+                }
+                catch {
+                    completion(Result(error: .responseParseError(error)))
+                }
+            default:
+                fatalError("invalid response combination \(data), \(response), \(error)")
+            }
+        }
+        task.resume()
     }
 }
