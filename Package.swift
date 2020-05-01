5870f8bd1b6972a42be6e1e83b4289e77f9db875
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2020-05-01 15:26:04 +0900

    Change package for 3rd edition

--- Package.swift
+++ Package.swift
@@ -1,22 +1,17 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "GithubSearchRepository",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    name: "github_search_repository",
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "GithubSearchRepository",
-            dependencies: []),
+            name: "GitHubSearch"),
         .testTarget(
-            name: "GithubSearchRepositoryTests",
-            dependencies: ["GithubSearchRepository"]),
+            name: "GitHubSearchTests",
+            dependencies: ["GitHubSearch"]),
+        .target(
+            name: "github_search_repository",
+            dependencies: ["GitHubSearch"]),
     ]
 )
